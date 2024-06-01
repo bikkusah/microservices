0d1aa63b92c573e91870127ee3eb0380dd6d99f2
bikkusah <87112266+bikkusah@users.noreply.github.com>
2024-06-01 15:18:08 +0530

    Update BaseEntity.java

--- section_17/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
+++ section_17/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -15,23 +15,23 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 //the purpose of this class is we need createdBy,bla bla in both table so we are creating ths class and we will use in our other tablble
 @MappedSuperclass //this class will act as super class and we can extend this class
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener.class)//go thr and create an object for By
 @Getter @Setter @ToString
 public class BaseEntity {
 
-    @CreatedDate
+    @CreatedDate//automatically handled by spring data jpa
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @CreatedBy
+    @CreatedBy//automatically handled by spring data jpa,for this we should write custom
     @Column(updatable = false)
     private String createdBy;
 
-    @LastModifiedDate
+    @LastModifiedDate//automatically hadnled by spring data jpa
     @Column(insertable = false)
     private LocalDateTime updatedAt;
 
-    @LastModifiedBy
+    @LastModifiedBy//automatically handled by spring data jpa,for this we should write custom
     @Column(insertable = false)
     private String updatedBy;
 }
