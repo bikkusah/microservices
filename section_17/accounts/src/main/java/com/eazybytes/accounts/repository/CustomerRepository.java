9fd8964c67c63a26982d6bae032862bab7b805df
bikkusah <87112266+bikkusah@users.noreply.github.com>
2024-06-01 12:33:18 +0530

    Update CustomerRepository.java

--- section_17/accounts/src/main/java/com/eazybytes/accounts/repository/CustomerRepository.java
+++ section_17/accounts/src/main/java/com/eazybytes/accounts/repository/CustomerRepository.java
@@ -9,7 +9,14 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    // This method tells Spring Data JPA to fetch a record from the customer table
+    // based on the value in the mobile_number column.
+    // Equivalent SQL: SELECT * FROM customer WHERE mobile_number = ?
     Optional<Customer> findByMobileNumber(String mobileNumber);
 
-
+    // If you want to fetch data based on multiple columns, you can define additional methods.
+    // For example, this method fetches a record based on values in both the mobile_number and column2 columns.
+    // Equivalent SQL: SELECT * FROM customer WHERE mobile_number = ? AND column2 = ?
+    // Uncomment the following method to use it:
+    // Optional<Customer> findByMobileNumberAndColumn2(String mobileNumber, String column2);
 }
