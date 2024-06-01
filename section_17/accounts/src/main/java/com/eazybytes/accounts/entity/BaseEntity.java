1ed0168c11cb308a3e78d03e302e9b6d4e848fd6
bikkusah <87112266+bikkusah@users.noreply.github.com>
2024-06-01 09:56:14 +0530

    Update BaseEntity.java

--- section_17/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
+++ section_17/accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -13,8 +13,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-
-@MappedSuperclass
+//the purpose of this class is we need createdBy,bla bla in both table so we are creating ths class and we will use in our other tablble
+@MappedSuperclass //this class will act as super class and we can extend this class
 @EntityListeners(AuditingEntityListener.class)
 @Getter @Setter @ToString
 public class BaseEntity {
