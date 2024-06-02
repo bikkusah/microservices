1ea25e3b9c42f2549d7697832d02727bd0507bc5
bikkusah <87112266+bikkusah@users.noreply.github.com>
2024-06-02 11:15:08 +0530

    Update AccountsContactInfoDto.java

--- section_17/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
+++ section_17/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
@@ -7,10 +7,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
+@ConfigurationProperties(prefix = "accounts")//prefix is accounts in both yaml and here
 @Getter
 @Setter
-public class AccountsContactInfoDto {
+public class AccountsContactInfoDto { we are using @Conf... to avoid @value bec its for single properties now we can inject multiple values from .yaml file
 
     private String message;
     private Map<String, String> contactDetails;
